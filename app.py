b36f5e27902957cfe645c68769dbf0eda8cd571d
felipefranca00 <felipe0franca907@gmail.com>
2026-06-15 15:05:08 -0300

    Adiciona rota de listagem de livros

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def home():
 def sobre():
     return "Sistema desenvolvido em Flask para estudo de CI/CD"
 
+@app.route("/livros")
+def livros():
+    return "Lista de livros cadastrados"
+
+
 
 
 if __name__ == "__main__":
